08cb0c8aa03db29fbdcd6e5360c0217f6656f762
Ahmed <sss.ahm7@gmail.com>
2023-05-01 14:51:01 +0500

    summarizer is complpeted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-4'>
+        <img className='w-28 object-contain' src={logo} alt="Logo" />
+
+        <button 
+        type='button'
+        onClick={() => window.open('https://github.com/ahmed20008')}
+        className='black_btn'
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className='head_text'>
+        Summarize Articals with <br className='max-md:hidden' />
+        <span className='orange_gradient'>Open AI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Sumzz. An Open Source articel summarizer that transform lengthly articles into clear and consise summaries. 
+      </h2>
+    </header>
   )
 }
 
